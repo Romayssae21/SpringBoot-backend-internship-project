4dca7d20ce7a21d652d1781ae963dd836e500f17
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-05 20:33:50 +0100

    Add a post method
    To save add and save a new team member

--- collaborateur/src/main/java/com/Romayssae/collaborateur/controller/TeamController.java
+++ collaborateur/src/main/java/com/Romayssae/collaborateur/controller/TeamController.java
@@ -1,6 +1,12 @@
 package com.Romayssae.collaborateur.controller;
 
+import com.Romayssae.collaborateur.dto.TeamDto;
 import com.Romayssae.collaborateur.entity.TeamMember;
+import com.Romayssae.collaborateur.service.TeamService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1")
 public class TeamController {
 
+    @Autowired
+    private TeamService service;
     @PostMapping("/save")
-    public String addTeamMember(@RequestBody TeamMember teamMember){
-
+    public ResponseEntity<TeamMember> saveTeamMember(@RequestBody @Valid TeamDto teamDto){
+        return new ResponseEntity<>(service.saveTeamMember(teamDto), HttpStatus.CREATED);
     }
 }
