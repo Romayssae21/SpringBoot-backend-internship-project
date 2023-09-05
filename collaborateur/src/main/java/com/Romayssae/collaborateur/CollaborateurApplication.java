21aadbdbd049ac6b8feff5409e3948dd5b59cf62
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-05 12:24:02 +0100

    postgreSQL configuration

---
+++ collaborateur/src/main/java/com/Romayssae/collaborateur/CollaborateurApplication.java
@@ -0,0 +1,13 @@
+package com.Romayssae.collaborateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborateurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborateurApplication.class, args);
+	}
+
+}
---
+++ collaborateur/src/test/java/com/Romayssae/collaborateur/CollaborateurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Romayssae.collaborateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborateurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
