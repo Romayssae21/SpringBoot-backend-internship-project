39e0ccfbb0ffe9bfddb83b899345ad9e9d2a2bd6
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-05 17:46:58 +0100

    Add an exception class
    To throw exceptions when needed

---
+++ collaborateur/src/main/java/com/Romayssae/collaborateur/exceptionHandler/TeamMemberIdNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Romayssae.collaborateur.exceptionHandler;
+
+public class TeamMemberIdNotFoundException extends Exception{
+    public TeamMemberIdNotFoundException(String message){
+        super(message);
+    }
+}
