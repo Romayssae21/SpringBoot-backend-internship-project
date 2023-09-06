4007ace1851fbf1ee2cd93065b9b42a2c990cc1b
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-06 12:14:08 +0100

    Add a security layer
    Adding some new dependencies that we will use and create a default User in the system & a custom password

---
+++ collaborateur/src/main/java/com/Romayssae/collaborateur/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.Romayssae.collaborateur.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    //Default User in the system & a custom password
+    public InMemoryUserDetailsManager user(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("RomaChan")
+                        .password("DigInErVa2à2#")
+                        .authorities("read")
+                        .build()
+        );
+    }
+
+
+}
