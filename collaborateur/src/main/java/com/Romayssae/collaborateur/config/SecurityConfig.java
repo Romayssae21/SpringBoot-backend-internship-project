c39e207ff03eaa63d89e8d04b04f475fc7930c4a
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-06 22:14:44 +0100

    Basic security layer
    with having users in memory so if the user is already authenticated he doesn't need to do it again

--- collaborateur/src/main/java/com/Romayssae/collaborateur/config/SecurityConfig.java
+++ collaborateur/src/main/java/com/Romayssae/collaborateur/config/SecurityConfig.java
@@ -2,15 +2,20 @@ package com.Romayssae.collaborateur.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     //Default User in the system & a custom password
@@ -25,23 +30,21 @@ public class SecurityConfig {
         );
     }
 
-    //Define other user:
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    //Basic security layer
     @Bean
-    public HttpSecurity securityFilterChain(HttpSecurity http)throws Exception{
+    SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         return http
-                .formLogin(form -> form
-                        .loginPage("/api/v1")
-                        .permitAll()
-                );
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authorize)->
+                        authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
                 //.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-                //.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                //.build();
+                .build();
 
     }
 
